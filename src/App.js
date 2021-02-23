76de2b8c0ce2f2787395dae39a05405891402a5f
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2021-02-23 19:38:58 +0530

    Base setup + Add pkg dependencies
    - Add react router, redux, redux dev tools packages
    - Add base route with "HomeScreen" component render
    - Setup prettier and workspace settings for vsCode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+
+// Custom component(s) import(s)
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* TODO: Render Header here */}
+      <Route path="/" exact component={HomeScreen}></Route>
+      {/* TODO: Render Footer here */}
+    </Router>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return <h1>HomeScreeen</h1>;
+};
+
+export default HomeScreen;
