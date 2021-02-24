35e1774307213161a81f08bd47b8642bbaec3839
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2021-02-24 10:30:41 +0530

    Add function descriptions for utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
+// Checks for required attributes of a function
 export const isRequired = arg => {
   throw new Error(`${arg} is a required argument.`);
 };
 
+/**
+ * @description Custom hook that triggers a callback if use clicks outside the given element (ref)
+ * @param {*} ref required
+ * @param {*} callback required
+ */
 export const useOutsideAlerter = (
   ref = isRequired('ref'),
   callback = isRequired('callback')
@@ -16,7 +22,7 @@ export const useOutsideAlerter = (
 
     document.addEventListener('mousedown', handleOutsideClick);
 
-    // Clean up the evenet created in this useEffect
+    // Clean up the event created in this useEffect
     return () => {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
